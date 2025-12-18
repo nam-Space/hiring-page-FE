bd550aeaa2b9de3ea07d53c0f6d27975df5ca779
PTITer2003 <namyahoo2003@gmail.com>
2025-12-18 15:26:47 +0700

    change /.app to /.App

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app';
 import { Provider } from 'react-redux'
 import { store } from '@/redux/store';
+import App from './App';
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
